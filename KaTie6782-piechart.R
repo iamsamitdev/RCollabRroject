8aba376da8cf35288a397077af989ab8f2493f29
KT wimonrat <katesarapornj@gmail.com>
2021-08-20 14:17:43 +0700

    add piechart to program

---
+++ KaTie6782-piechart.R
@@ -0,0 +1,5 @@
+# Pie Chart from data frame with Appended Sample Sizes
+mytable <- table(iris$Species)
+lbls <- paste(names(mytable), "\n", mytable, sep="")
+pie(mytable, labels = lbls,
+    main="Pie Chart of Species\n (with sample sizes)")
